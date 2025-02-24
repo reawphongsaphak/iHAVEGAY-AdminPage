2277edbafe9e9be5b7ac3bbae7e5914c4a65c83c
reawphongsaphak <reawza00789@gmail.com>
2025-02-24 22:39:40 +0700

    install tailwind

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,9 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-80xl font-bold underline">
+      Hello world!
+  </h1>
 </template>
 
 <style scoped>
