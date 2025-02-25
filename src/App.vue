e3a5daf71e79cc05fc2887ef70b5a6129c1ed98e
HUTZAKI <sirapatpanmoon@gmail.com>
2025-02-25 19:24:27 +0700

    navigation-fixed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="flex flex-col h-screen">
     <!-- Top Navigation Bar -->
-    <navigation />
+    <navigation @user-manage-clicked="hideSubmenu" @hardware-clicked="showSubmenu" />
 
     <div class="flex flex-1">
       <!-- Sidebar (Submenu) -->
-      <div>
+      <div v-if="isSubmenuVisible">
         <submenu />
       </div>
 
       <!-- Main Content (Dynamic Pages) -->
-      <div class=" bg-white">
+      <div class="bg-white">
         <router-view />
         <!-- <HelloWorldVue /> -->
       </div>
@@ -19,9 +19,22 @@
 </template>
 
 <script setup>
-// import HelloWorldVue from './components/HelloWorld.vue'
+import { ref } from 'vue'
 import navigation from './components/navigation.vue'
 import submenu from './components/submenu.vue'
+
+// Reactive variable to control submenu visibility
+const isSubmenuVisible = ref(true)
+
+// Method to hide the submenu
+const hideSubmenu = () => {
+  isSubmenuVisible.value = false
+}
+
+// Method to show the submenu again
+const showSubmenu = () => {
+  isSubmenuVisible.value = true
+}
 </script>
 
 <style>
--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,25 +1,28 @@
 <template>
-  <div class="navbar bg-green-500">
+  <div class="navbar bg-green-500 shadow-lg px-4 py-2">
+    <!-- Left Side Dropdown -->
     <div class="flex-none">
-      <button class="btn btn-square btn-ghost">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="inline-block h-5 w-5 stroke-current">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"></path>
-        </svg>
-      </button>
+      <details class="dropdown">
+        <summary class="btn m-1 text-white hover:bg-green-600 rounded-lg transition-all">Change</summary>
+        <ul class="menu dropdown-content bg-white text-black rounded-box z-[1] w-52 p-2 shadow-md">
+          <router-link to="/user" @click="handleUserManageClick">
+            <li><a class="hover:bg-gray-300 active:bg-gray-400">User Manage</a></li>
+          </router-link>
+          <router-link to="/" @click="handleHardwareClick">
+            <li><a class="hover:bg-gray-300 active:bg-gray-400">Hardware</a></li>
+          </router-link>
+        </ul>
+      </details>
     </div>
-    <div class="flex-1">
-      <a class="btn btn-ghost text-xl text-white">IHaveGAY</a>
+
+    <!-- Center Logo/Title -->
+    <div class="flex-1 text-center">
+      <a class="btn btn-ghost text-2xl font-semibold text-white hover:bg-green-600 rounded-lg transition-all">IHaveGAY</a>
     </div>
+
+    <!-- Right Side Icon/Button -->
     <div class="flex-none">
-      <button class="btn btn-square btn-ghost">
+      <button class="btn btn-square btn-ghost hover:bg-green-600 rounded-lg transition-all">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -38,10 +41,15 @@
 
 <script>
 export default {
-
+  methods: {
+    handleUserManageClick() {
+      // Emit an event to hide the submenu when User Manage is clicked
+      this.$emit('user-manage-clicked')
+    },
+    handleHardwareClick() {
+      // Emit an event to show the submenu when Hardware is clicked
+      this.$emit('hardware-clicked')
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
